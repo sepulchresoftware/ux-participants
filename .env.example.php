a960022b35743dd03d6f3d6309d42203aa8536e5
Matthew Fritz <mattf@burbankparanormal.com>
2015-01-29 11:33:35 -0800

    Added the default value for timezone

--- .env.example.php
+++ .env.example.php
@@ -23,7 +23,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-    'TIMEZONE'                 => 'America/Los_Angeles',
+    'TIMEZONE'                 => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
