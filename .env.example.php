c8ac1baeb333e6ccba2e2eafd066d8c08c46a689
Matthew Fritz <mattf@burbankparanormal.com>
2015-01-28 18:26:27 -0800

    Added the example env file

---
+++ .env.example.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'DEBUG'                    => true,
+    'DB_HOST'                  => 'example',
+    'DB_NAME'                  => 'example',
+    'DB_USERNAME'              => 'example',
+    'DB_PASSWORD'              => 'example',
+    'DB_PORT'                  => 'example',
+    'LDAP_HOST'                => 'ldap.example.com',
+    'LDAP_BASE_DN'             => 'example',
+    'LDAP_DN'                  => 'example',
+    'LDAP_PASSWORD'            => 'example',
+    'LDAP_ALLOW_NO_PASS'       => false,
+    'SMTP_HOST'                => 'smtp.example.com',
+    'SMTP_USERNAME'            => 'example',
+    'SMTP_PASSWORD'            => 'example',
+    'SMTP_ENCRYPTION'          => 'example',
+    'SMTP_PORT'                => 'example',
+];
