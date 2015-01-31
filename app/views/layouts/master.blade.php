afd064b2798e8d2e09647d8dc4443f44f6c92e5c
Matthew Fritz <mattf@burbankparanormal.com>
2015-01-31 10:41:17 -0800

    Added a basic navbar to the system

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
 
 	<link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" />
 	<link rel="stylesheet" href="{{ asset('bower_components/components-font-awesome/css/font-awesome.min.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
 
 	<script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,3 +1,45 @@
 <!-- Begin Navigation -->
 
+<nav class="navbar navbar-inverse navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="{{ url('/') }}">UX Participants</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse navbar-ex1-collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="{{ url('/') }}">Home</a></li>
+
+			@if (Auth::check())
+			<li><a href="#">Studies</a></li>
+			@endif
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			@if (Auth::check())
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{{ Auth::user()->name }}} <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">My Studies</a></li>
+					<li><a href="#">My Profile</a></li>
+					@if (Auth::user()->isAdmin())
+					<li><a href="#">Admin Panel</a></li>
+					@endif
+					<li role="presentation" class="divider"></li>
+					<li><a href="{{ url('auth/logout') }}">Logout</a></li>
+				</ul>
+			</li>
+			@else
+			<li><a href="{{ url('auth/login') }}">Login</a></li>
+			@endif
+		</ul>
+	</div><!-- /.navbar-collapse -->
+</nav>
+
 <!-- End Navigation -->
