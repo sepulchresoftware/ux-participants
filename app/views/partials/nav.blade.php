ebaa03eb923332d480fbb2cd49091d13d8eec9aa
Matthew Fritz <mattf@burbankparanormal.com>
2015-02-02 00:28:15 -0800

    Modified the way the Admin Panel is displayed on the drop-down menu

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -15,7 +15,6 @@
 	<!-- Collect the nav links, forms, and other content for toggling -->
 	<div class="collapse navbar-collapse navbar-ex1-collapse">
 		<ul class="nav navbar-nav">
-
 			@if (Auth::check())
 			<li><a href="#">Studies</a></li>
 			@endif
@@ -25,11 +24,11 @@
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{{ Auth::user()->name }}} <b class="caret"></b></a>
 				<ul class="dropdown-menu">
-					<li><a href="#">My Studies</a></li>
-					<li><a href="#">My Profile</a></li>
 					@if (Auth::user()->isAdmin())
 					<li><a href="#">Admin Panel</a></li>
 					@endif
+					<li><a href="#">My Studies</a></li>
+					<li><a href="#">My Profile</a></li>
 					<li role="presentation" class="divider"></li>
 					<li><a href="{{ url('auth/logout') }}">Logout</a></li>
 				</ul>
