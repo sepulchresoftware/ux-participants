29c30c589909ee99499119c43d79f2c8ef5e4e1c
Matthew Fritz <mattf@burbankparanormal.com>
2015-02-05 12:51:01 -0800

    Started working on the Calendar handler

---
+++ app/handlers/HandlerCalendar.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Handler class for Calendar operations.
+ */
+class HandlerCalendar
+{
+	private $months;
+
+	private $days;
+
+	private $disableWeekends;
+
+	/**
+	 * Constructs a new HandlerCalendar class.
+	 */
+	public function __construct() {
+		$this->disableWeekends = true;
+		
+		$this->days = array(
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		);
+
+		$this->months = array(
+			array(
+				"name" => "January",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "February",
+				"days" => 28,
+				"leapdays" => 29,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "March",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "April",
+				"days" => 30,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "May",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "June",
+				"days" => 30,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "July",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "August",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "September",
+				"days" => 30,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "October",
+				"days" => 31,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "November",
+				"days" => 30,
+				"holidays" => array(),
+			),
+			array(
+				"name" => "December",
+				"days" => 31,
+				"holidays" => array(),
+			),
+		);
+	}
+
+	/**
+	 * Set whether weekends are disabled on the calendar.
+	 *
+	 * @param boolean $disable Whether to disable weekends
+	 */
+	public function disableWeekends($disable) {
+		$this->disableWeekends = $disable;
+	}
+
+	/**
+	 * Returns the array containing names of days.
+	 *
+	 * @return array:string
+	 */
+	public function getDays() {
+		return $this->days;
+	}
+
+	/**
+	 * Returns an array containing arrays for each month.
+	 *
+	 * @return array:array
+	 */
+	public function getMonths() {
+		return $this->months;
+	}
+
+	/**
+	 * Returns whether weekends are disabled on the calendar.
+	 *
+	 * @return boolean
+	 */
+	public function weekendsDisabled() {
+		return $this->disableWeekends;
+	}
+}
