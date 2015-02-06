c016f2414a215e3966860449090455ea6dfbf5ae
Matthew Fritz <mattf@burbankparanormal.com>
2015-02-05 19:37:23 -0800

    Added better default sizing for the table on the My Studies page

--- app/views/pages/users/studies.blade.php
+++ app/views/pages/users/studies.blade.php
@@ -13,7 +13,7 @@ My Studies
 <div class="row">
 
 	@if ($studies->count() > 0)
-		<div class="col-sm-8 col-sm-offset-2">
+		<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
 
 			<table class="table">
 				<thead>
